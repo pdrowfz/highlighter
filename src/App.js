498b25751c6af26381e9df956feb14d98400d2e8
Pedro Zanetti <pdrowfz@gmail.com>
2019-11-05 20:41:09 -0300

    Setting up ESLint, Prettier, EditorConfig, Redux, Reactotron and Root Import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
+import store from './store';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Provider store={store}>
+      <div className="App" />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import highlight from './highlight/reducer';
+
+export default combineReducers({
+  highlight,
+});
