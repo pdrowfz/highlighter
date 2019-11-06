74ddbc40570cc48ad514f4e9d38930f973451e9a
Pedro Zanetti <pdrowfz@gmail.com>
2019-11-05 22:18:35 -0300

    Adding global styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
+import GlobalStyle from './styles/global';
+
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App" />
+      <GlobalStyle />
     </Provider>
   );
 }
